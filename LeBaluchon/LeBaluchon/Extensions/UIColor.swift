2bfdd1e2d978cb2ee83ede4c33a084e8e415187b
Maxime ROUX <r.maxime@yandex.com>
2021-09-10 15:36:02 +0200

    UIColor

---
+++ LeBaluchon/LeBaluchon/Extensions/UIColor.swift
@@ -0,0 +1,14 @@
+//
+//  UIColor.swift
+//  Le Baluchon
+//
+//  Created by ROUX Maxime on 04/09/2021.
+//
+
+import UIKit
+
+extension UIColor {
+    public class var deepBlue: UIColor {
+        return UIColor(red: 8/255, green: 43/255, blue: 118/255, alpha: 1.0)
+    }
+}
