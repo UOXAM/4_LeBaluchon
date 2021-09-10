79e0446ae144ad0ba290e4c2d8e8074608620cb7
Maxime ROUX <r.maxime@yandex.com>
2021-09-10 15:12:36 +0200

    Project Xcode with MVC pattern

---
+++ LeBaluchon/LeBaluchon/Sources/Controller/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  LeBaluchon
+//
+//  Created by ROUX Maxime on 10/09/2021.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
