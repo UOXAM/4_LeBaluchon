6487a0438db79412e2ba86495fc2ceac2fe2e037
Maxime ROUX <r.maxime@yandex.com>
2021-09-10 17:08:19 +0200

    CurrencyViewController: UIPickerView Protocol, info.plist : allow http unsafe

--- LeBaluchon/LeBaluchon/Sources/Controller/CurrencyViewController.swift
+++ LeBaluchon/LeBaluchon/Sources/Controller/CurrencyViewController.swift
@@ -7,7 +7,7 @@
 
 import UIKit
 
-class CurrencyWelcomeViewController: UIViewController {
+class CurrencyViewController: UIViewController {
     
     // MARK: - Outlets
     
@@ -15,19 +15,18 @@ class CurrencyWelcomeViewController: UIViewController {
     @IBOutlet weak var toAmountLabel: UILabel!
     @IBOutlet weak var finalCurrencyPickerView: UIPickerView!
     @IBOutlet weak var initialCurrencyPickerView: UIPickerView!
+    
     @IBOutlet weak var activityIndicator: UIActivityIndicatorView!
+    
     @IBOutlet weak var conversionButton: UIButton!
     @IBOutlet weak var initialCurrencyStackView: UIStackView!
     @IBOutlet weak var finalCurrencyStackView: UIStackView!
     @IBOutlet weak var dateCurrencyRateLabel: UILabel!
     @IBOutlet weak var dateActualizationStackView: UIStackView!
-    
-    
+            
     // MARK: - Properties
-    
     private let exchangeService = CurrencyService()
     private var currencyListArray: [String] = []
-
     
     // MARK: - viewDidLoad & Network call (currency list)
     
@@ -96,11 +95,80 @@ class CurrencyWelcomeViewController: UIViewController {
         }
     }
     
+    
     // MARK: - Action & Network
-
+    
     @IBAction func conversionButton(_ sender: UIButton) {
+        
+        // Properties
+        let initialCurrencyCode : String = getCurrencyCode(pickerView: initialCurrencyPickerView)
+        let finalCurrencyCode : String = getCurrencyCode(pickerView: finalCurrencyPickerView)
+        
+        guard let initialAmount :Double = Double(fromAmountTextField.text!) else {return}
 
+        // Network call to keep currency rates
+        exchangeService.fetchCurrencyRates(initialCurrency: initialCurrencyCode, finalCurrency: finalCurrencyCode) { [weak self] result in
+
+            DispatchQueue.main.async {
+                self?.activityIndicator.startAnimating()
+                self?.conversionButton.isEnabled = false
+                switch result {
+                case .success(let currencyRates):
+                                        
+                    // Search the rate of the initial currency in the result of the network call
+                    guard let initialRate :Double = Double?(currencyRates.rates[initialCurrencyCode]!) else {return}
+                    // Search the rate of the final currency in the result of the network call
+                    guard let finalRate :Double = currencyRates.rates[finalCurrencyCode] else {return}
+
+                    // Calculate the result amount converted
+                    let result = self?.exchangeService.calculateAmount(initialAmount: initialAmount, initialRate: initialRate, finalRate: finalRate)
+                    // Show the result with 2 decimals
+                    self?.toAmountLabel.text = String(format: "%.02f", result!)
+                    self?.dateCurrencyRateLabel.text = String("\(currencyRates.date) ")
+                    print(currencyRates.date)
+                    self?.activityIndicator.stopAnimating()
+
+                case.failure(let error):
+                    self?.showAlert(with: error.description)
+                    self?.activityIndicator.stopAnimating()
+                }
+                self?.conversionButton.isEnabled = true
+                self?.activityIndicator.stopAnimating()
+            }
+        }
+    }
+    
+    private
+    func getCurrencyCode(pickerView: UIPickerView?) -> String {
+        guard let currencyIndex = pickerView?.selectedRow(inComponent: 0) else {return ""}
+        let currency :String = currencyListArray[currencyIndex]
+        return currency.components(separatedBy: " : ")[0]
+    }
+    
+}
+
+    // MARK: - PickerView
+
+extension CurrencyViewController : UIPickerViewDelegate, UIPickerViewDataSource {
+        
+    // Number of elements to show (column)
+        
+    func numberOfComponents(in pickerView: UIPickerView) -> Int {
+        return 1
+    }
+                
+    // Number of rows
+    func pickerView(_ pickerView: UIPickerView, numberOfRowsInComponent component: Int) -> Int {
+        return currencyListArray.count
+    }
+    
+    func pickerView(_ pickerView: UIPickerView, attributedTitleForRow row: Int, forComponent component: Int) -> NSAttributedString? {
+
+        let string = currencyListArray[row]
+        return NSAttributedString(string: string, attributes: [NSAttributedString.Key.foregroundColor: UIColor.deepBlue])
     }
     
 
+
+        
 }
