7f51e44e7bfcfa6e7af053732f91eab7a8ec8453
Maxime ROUX <r.maxime@yandex.com>
2021-09-10 15:34:23 +0200

    URLSession+GenericCall, NetworkError, NetworkLogger

---
+++ LeBaluchon/LeBaluchon/Sources/Model/Network/NetworkError.swift
@@ -0,0 +1,21 @@
+//
+//  NetworkError.swift
+//  Le Baluchon
+//
+//  Created by ROUX Maxime on 23/08/2021.
+//
+
+import Foundation
+
+enum NetworkError:  Error {
+    case noData, invalidResponse, undecodableData
+}
+
+extension NetworkError: CustomStringConvertible {
+    var description: String {
+        switch self {
+        case .noData, .invalidResponse, .undecodableData:
+            return "The service is momentarily unavailable"
+        }
+    }
+}
